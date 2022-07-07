8f165b81c5f6f655693ab7edf57a2f87cf0d01ab
prabhakaran_mrk <78244850+prabhakaran-mrk@users.noreply.github.com>
2022-07-07 22:48:18 +0530

    Delete cylinder.js

--- cylinder.js
+++
@@ -1,8 +0,0 @@
-function calculateArea(){
-	var r1=document.getElementById('rad1').value;
-	var h=document.getElementById('heigh1').value;
-	var Area = 2 * 3.14 * parseInt(r1)* h + 2 * 3.14 * parseInt(r1) * parseInt(r1) ;
-	var Volume = parseFloat(3.14) *  parseInt(r1) *  parseInt(r1) * h;
-		document.getElementById('res1').value = Area;
-		document.getElementById('res2').value = Volume;
-}
