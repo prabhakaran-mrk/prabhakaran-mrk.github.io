a1a9f6759498a9a1c0084eac151d5d5f3ce6c183
prabhakaran_mrk <78244850+prabhakaran-mrk@users.noreply.github.com>
2022-07-07 22:51:57 +0530

    file added
    file added

---
+++ cylinder.js
@@ -0,0 +1,8 @@
+function calculateArea(){
+	var r1=document.getElementById('rad1').value;
+	var h=document.getElementById('heigh1').value;
+	var Area = 2 * 3.14 * parseInt(r1)* h + 2 * 3.14 * parseInt(r1) * parseInt(r1) ;
+	var Volume = 3.14 *  parseInt(r1) *  parseInt(r1) * h;
+		document.getElementById('res1').value = Area;
+		document.getElementById('res2').value = Volume;
+}
